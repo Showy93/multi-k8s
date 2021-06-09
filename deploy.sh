2c014e26d72985d6128fe9ffe9ce867bf61a398b
Marcin Korycki <mkorycki93@gmail.com>
2021-06-09 21:28:27 +0200

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t showy93/multi-client:latest -t showy93/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t showy93/multi-server:latest -t showy93/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t showy93/multi-worker:latest -f showy93/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push showy93/multi-client:latest
+docker push showy93/multi-server:latest
+docker push showy93/multi-worker:latest
+docker push showy93/multi-client:$SHA
+docker push showy93/multi-server:$SHA
+docker push showy93/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=showy93/multi-server:$SHA
+kubectl set image deployments/client-deployment client=showy93/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=showy93/multi-worker:$SHA
